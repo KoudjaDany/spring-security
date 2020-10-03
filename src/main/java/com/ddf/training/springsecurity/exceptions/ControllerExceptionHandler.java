cceae95f0a5b8c5c6d64b8180f84daf2a3864f30
Dany Koudja <koudjadany@gmail.com>
2020-10-03 07:20:22 +0200

    Adding ExceptionHandler and Spring Security dependence to project.

--- src/main/java/com/ddf/training/springsecurity/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/ddf/training/springsecurity/exceptions/ControllerExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.NoSuchElementException;
@@ -14,7 +13,6 @@ public class ControllerExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NoSuchElementException.class)
-    @ResponseBody()
     public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException exception){
         return ResponseEntity.ok(exception.getMessage());
     }
